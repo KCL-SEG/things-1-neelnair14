8dec575e97522685845aaf082c7b3ed89f30671d
Neel Nair <neel14nair@gmail.com>
2023-12-05 14:54:32 +0000

    Fix issues with tests

--- things/models.py
+++ things/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(unique=True, blank=False,max_length=30)
     description = models.CharField(unique = False, blank = True, max_length = 120)
-    quantity = models.IntegerField(unique = False, blank = True, max_length= 100)
+    quantity = models.IntegerField(unique = False, blank = True, validators=[
+            MinValueValidator(0, message="Quantity must be at least 0."),
+            MaxValueValidator(100, message="Quantity must be at most 100."),
+        ])
