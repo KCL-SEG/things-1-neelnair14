e672e6f502171e91cb4663fea2213a21e74476ca
Neel Nair <neel14nair@gmail.com>
2023-12-05 14:43:17 +0000

    create Things model and extend it

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(unique=True, blank=False,max_length=30)
+    description = models.CharField(unique = False, blank = True, max_length = 120)
+    quantity = models.IntegerField(unique = False, blank = True, max_length= 100)
